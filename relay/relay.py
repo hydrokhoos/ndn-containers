462bb6bcf25ff322b766ff279a47d83ff5db1e9b
hydrokhoos <3110hideyuki@gmail.com>
2022-07-08 19:51:30 +0900

    update[

--- relay/relay.py
+++ relay/relay.py
@@ -1,23 +1,27 @@
 from ndn.app import NDNApp
 from ndn.encoding import Name
-from ndn.types import InterestNack, InterestTimeout, InterestCanceled, ValidationFailure
+from ndn.types import (InterestCanceled, InterestNack, InterestTimeout,
+                       ValidationFailure)
 
 
 app = NDNApp()
 
 service_name = '/relay'
-l = []
+
+relay_info = {'original_interest_name': '', 'trim_interest_name': '', 'original_data': b'', 'relaid_data': b''}
+
 
 def receive_interest():
     @app.route(service_name)
     def on_interest(name, param, _app_param):
+        print(f'Received Interest:\t{Name.to_str(name)}')
+
         # remove '/relay'(service_name) from interest name
-        print(f'Received Interest: {Name.to_str(name)}')
-        tmp_name = (Name.to_str(name)).replace(service_name + '/', '/')
-        name = Name.from_str(tmp_name)
-        l.append(tmp_name)
+        relay_info['original_interest_name'] = Name.to_str(name)
+        relay_info['trim_interest_name'] = (Name.to_str(name)).replace(service_name + '/', '/')
 
         app.shutdown()
+
 
     print('Start receiver ...')
     app.run_forever()
@@ -26,11 +30,10 @@ def receive_interest():
 def send_interest():
     async def main():
         try:
-            print(f'Sending Interest: {Name.to_str(name)}')
+            print(f'Sending Interest:\t{Name.to_str(name)}')
             data_name, meta_info, content = await app.express_interest(name, must_be_fresh=True, can_be_prefix=False, lifetime=6000)
-            print(f'Received Data: {Name.to_str(data_name)}')
-            # print(meta_info)
-            # print(bytes(content) if content else None)
+            print(f'Received Data Name:\t{Name.to_str(data_name)}')
+            relay_info['original_data'] = bytes(content) if content else None
         except InterestNack as e:
             print(f'Nacked with reason={e.reason}')
         except InterestTimeout:
@@ -40,17 +43,33 @@ def send_interest():
         except ValidationFailure:
             print(f'Data failed to validate')
         finally:
-            data_name = service_name + Name.to_str(data_name)
-            print(f'Put Data: {Name.to_str(data_name)}')
-            app.put_data(data_name, content=content, freshness_period=10000)
             app.shutdown()
 
-    name = Name.from_str(l[0])
+
+    name = Name.from_str(relay_info['trim_interest_name'])
 
     app.run_forever(after_start=main())
 
 
+def send_data():
+    async def main():
+        print(f'Putting Data Name:\t{relay_info["original_interest_name"]}')
+        app.put_data(Name.from_str(relay_info['original_interest_name']), content=relay_info['relaid_data'], freshness_period=100000)
+        app.shutdown()
+
+
+    app.run_forever(after_start=main())
+
 
 if __name__ == '__main__':
     receive_interest()
+
     send_interest()
+
+    # just relay
+    relay_info['relaid_data'] = relay_info['original_data']
+
+    send_data()
+
+    # print()
+    # print(relay_info)
