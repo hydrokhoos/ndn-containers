e1f25583bca1b2fb57b0609bb171ea47879b1519
hydrokhoos <3110hideyuki@gmail.com>
2022-07-13 15:55:05 +0900

    fix typo

--- relay/shell/docker_consumer.sh
+++ relay/shell/docker_consumer.sh
@@ -1,5 +1,5 @@
 nfd-start
 ndnsec key-gen /root | ndnsec cert-install -
 nfdc face create udp://relay.testNet
-nfdc route add /ndn udp://relay.testNetrelay
+nfdc route add /ndn udp://relay.testNet
 nfdc route add prefix / nexthop udp://relay.testNet
